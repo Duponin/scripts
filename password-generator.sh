8f8ea4fc8a2c04131eeab9f525c53469bb9104d0
Duponin <duponin@gmail.com>
2018-11-02 03:15:46 +0100

    Takes arguments to format ouput length and iteration

--- password-generator.sh
+++ password-generator.sh
@@ -7,12 +7,35 @@ mkdir -p $PWDIR
 cd $PWDIR
 dd if=/dev/urandom of=raw bs=1K count=1 status=none
 base64 raw > translated.txt
-read -p "Enter password lenght desired: " PWLEN
-if test $PWLEN -ge $MXLEN
+
+if [ -z "$1"  ]
 then
-    PWLEN=$MXLEN
+    read -p "Enter password lenght desired: " PWLEN
+    if [ -z "$PWLEN" ]
+    then
+        PWLEN=$MXLEN
+    elif test $PWLEN -ge $MXLEN
+    then
+        PWLEN=$MXLEN
+    else
+        PWLEN=$((PWLEN+1))
+    fi
 else
-    PWLEN=$((PWLEN+1))
+    PWLEN=$1
+    if test $PWLEN -ge $MXLEN
+    then
+        PWLEN=$MXLEN
+    else
+        PWLEN=$((PWLEN+1))
+    fi
 fi
+
 cat translated.txt | colrm $PWLEN > translated-trimmed.txt
-cat translated-trimmed.txt
+
+if [[ $2 ]]
+then
+    cat translated-trimmed.txt | head --lines=$2
+else
+    cat translated-trimmed.txt
+fi
+
